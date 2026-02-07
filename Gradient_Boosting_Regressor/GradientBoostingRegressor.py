f9387e07abd745e1dfc55e9406d18bc059b19e29
zjzhao1002 <98807606+zjzhao1002@users.noreply.github.com>
2026-02-07 17:09:23 +0800

    Add docstrings to GradientBoostingRegressor class
    Added docstrings to the GradientBoostingRegressor methods.

--- Gradient_Boosting_Regressor/GradientBoostingRegressor.py
+++ Gradient_Boosting_Regressor/GradientBoostingRegressor.py
@@ -3,12 +3,25 @@ from DecisionTreeRegressor import DecisionTreeRegressor
 
 class GradientBoostingRegressor():
     def __init__(self, n_estimators: int=2, learning_rate: float=0.3, max_depth: int=2):
+        """
+        The constructor for GradientBoostingRegressor class.
+        Args:
+            n_estimarots: The number of weak models in the model.
+            learning_rate: The learning rate.
+            max_depth: The maximum depth of the decision tree.
+        """
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.max_depth = max_depth
         self.trees = []
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+        """
+        This function builds and fits the model to the given feature matrix and target values.
+        Args:
+            X: The feature matrix.
+            y: The target values.
+        """
         self.initial_leaf = np.mean(y)
         predictions = np.zeros(y.shape) + self.initial_leaf
         
@@ -20,6 +33,13 @@ class GradientBoostingRegressor():
             self.trees.append(tree)
 
     def predict(self, X: np.ndarray):
+        """
+        This function combines the predictions of weak models and give the final predictions.
+        Args: 
+            X: The feature matrix to make predictions for.
+        Returns:
+            A numpy array of predicted target values.
+        """
         predictions = np.zeros(shape=(len(X), 1)) + self.initial_leaf
         
         for i in range(self.n_estimators):
